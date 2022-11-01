4bc47d2e052a284f33d5d272671fea3706bf4617
Patrick Stigler <patrick@stigler.de>
2022-11-01 01:11:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 app = FastAPI()
+import subprocess
 
 @app.get("/")
 async def root():
-    return {"Uvicorn": "I'm alive"}
+    return {"Server": "I'm alive"}
+    
+@app.get("/dl/{id_link}")
+async def root():
+    cmd = 'tidal-dl -l {id_link}'
+    sp = subprocess.Popen(cmd,shell=True)
+    rc=sp.wait()
+    return {"Server": sp}
